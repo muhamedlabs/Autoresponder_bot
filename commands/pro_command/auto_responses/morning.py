7ff726599ee758e9dcb7f68a3711bb7403fe264e
Muhamed Ads <128980327+muhamedlabs@users.noreply.github.com>
2025-02-12 14:35:09 +0200

    Update morning.py

--- commands/pro_command/auto_responses/morning.py
+++ commands/pro_command/auto_responses/morning.py
@@ -3,5 +3,7 @@ import asyncio
 async def auto_reply(event):
     """Автоответ на слово 'утро'."""
     await asyncio.sleep(5)
-    await event.respond("Доброе утро! ☀️")
+    await event.respond("Утро — это начало нового дня!")
+    print(f"Ответ на 'утро' в чате {event.chat_id}")
+
 
